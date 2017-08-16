6d937bc96cb17e018d6c89630d52b7fd75799400
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-08-16 06:08:38 +0000

    fixed some names, commented out carousel

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function college() {
                     var webPath = json.results[i].school.school_url;
                     var websitePath = document.createElement("a");
                     websitePath.setAttribute("id", "web");
-                    websitePath.setAttribute('class', "font")
+                    websitePath.setAttribute('class', "font");
                     websitePath.setAttribute('href', `https://${webPath}`);
                     websitePath.innerHTML = webPath;
 
@@ -56,92 +56,92 @@ function college() {
 
                     //Carousel
 
-                    var carousel = document.createElement("div");
-                    carousel.setAttribute("class", "container carousel");
+                    // var carousel = document.createElement("div");
+                    // carousel.setAttribute("class", "container carousel");
 
-                    carousel.setAttribute("id", "carouselthing");
-                    var carousel2 = document.createElement("div");
-                    carousel2.setAttribute("class", "carousel slide");
-                    carousel2.setAttribute("id", "myCarousel" + i);
-                    carousel2.setAttribute("data-ride", "carousel");
-                    carousel.appendChild(carousel2);
+                    // carousel.setAttribute("id", "carouselthing");
+                    // var carousel2 = document.createElement("div");
+                    // carousel2.setAttribute("class", "carousel slide");
+                    // carousel2.setAttribute("id", "myCarousel" + i);
+                    // carousel2.setAttribute("data-ride", "carousel");
+                    // carousel.appendChild(carousel2);
 
-                    var ol = document.createElement("ol");
-                    ol.setAttribute("class", "carousel-indicators");
-                    carousel2.appendChild(ol);
+                    // var ol = document.createElement("ol");
+                    // ol.setAttribute("class", "carousel-indicators");
+                    // carousel2.appendChild(ol);
 
-                    var dot1 = document.createElement("li");
-                    dot1.setAttribute("data-target", "#myCarousel" + i);
-                    dot1.setAttribute("data-slide-to", "0");
-                    dot1.setAttribute("class", "active");
-                    ol.appendChild(dot1);
+                    // var dot1 = document.createElement("li");
+                    // dot1.setAttribute("data-target", "#myCarousel" + i);
+                    // dot1.setAttribute("data-slide-to", "0");
+                    // dot1.setAttribute("class", "active");
+                    // ol.appendChild(dot1);
 
-                    var dot2 = document.createElement("li");
-                    dot2.setAttribute("data-target", "#myCarousel" + i);
-                    dot2.setAttribute("data-slide-to", "1");
-                    ol.appendChild(dot2);
+                    // var dot2 = document.createElement("li");
+                    // dot2.setAttribute("data-target", "#myCarousel" + i);
+                    // dot2.setAttribute("data-slide-to", "1");
+                    // ol.appendChild(dot2);
 
-                    var dot3 = document.createElement("li");
-                    dot3.setAttribute("data-target", "#myCarousel" + i);
-                    dot3.setAttribute("data-slide-to", "2");
-                    ol.appendChild(dot3);
+                    // var dot3 = document.createElement("li");
+                    // dot3.setAttribute("data-target", "#myCarousel" + i);
+                    // dot3.setAttribute("data-slide-to", "2");
+                    // ol.appendChild(dot3);
 
-                    var carouselinner = document.createElement("div");
-                    carouselinner.setAttribute("class", "carousel-inner");
-                    carousel2.appendChild(carouselinner);
+                    // var carouselinner = document.createElement("div");
+                    // carouselinner.setAttribute("class", "carousel-inner");
+                    // carousel2.appendChild(carouselinner);
 
-                    var img1 = document.createElement("div");
-                    img1.setAttribute("class", "item active");
-                    carouselinner.appendChild(img1);
+                    // var img1 = document.createElement("div");
+                    // img1.setAttribute("class", "item active");
+                    // carouselinner.appendChild(img1);
 
-                    var img11 = document.createElement("img");
-                    img11.setAttribute("src", "kwklogozoom.png");
-                    img11.setAttribute("alt", "First Image");
-                    img11.setAttribute("style", "width:100%");
-                    img1.appendChild(img11);
+                    // var img11 = document.createElement("img");
+                    // img11.setAttribute("src", "kwklogozoom.png");
+                    // img11.setAttribute("alt", "First Image");
+                    // img11.setAttribute("style", "width:100%");
+                    // img1.appendChild(img11);
 
-                    var img2 = document.createElement("div");
-                    img2.setAttribute("class", "item");
-                    carouselinner.appendChild(img2);
+                    // var img2 = document.createElement("div");
+                    // img2.setAttribute("class", "item");
+                    // carouselinner.appendChild(img2);
 
-                    var img22 = document.createElement("img");
-                    img22.setAttribute("src", "https://kodewithklossydotcom.files.wordpress.com/2016/08/kwk-hero-image.jpg");
-                    img22.setAttribute("alt", "Second Image");
-                    img22.setAttribute("style", "width:100%");
-                    img2.appendChild(img22);
+                    // var img22 = document.createElement("img");
+                    // img22.setAttribute("src", "https://kodewithklossydotcom.files.wordpress.com/2016/08/kwk-hero-image.jpg");
+                    // img22.setAttribute("alt", "Second Image");
+                    // img22.setAttribute("style", "width:100%");
+                    // img2.appendChild(img22);
 
-                    var img3 = document.createElement("div");
-                    img3.setAttribute("class", "item");
-                    carouselinner.appendChild(img3);
-                    var img33 = document.createElement("img");
-                    img33.setAttribute("src", "karlie2zoom.jpg");
-                    img33.setAttribute("alt", "First Image");
-                    img33.setAttribute("style", "width:100%");
-                    img3.appendChild(img33);
+                    // var img3 = document.createElement("div");
+                    // img3.setAttribute("class", "item");
+                    // carouselinner.appendChild(img3);
+                    // var img33 = document.createElement("img");
+                    // img33.setAttribute("src", "karlie2zoom.jpg");
+                    // img33.setAttribute("alt", "First Image");
+                    // img33.setAttribute("style", "width:100%");
+                    // img3.appendChild(img33);
 
-                    var leftcontrol = document.createElement("a");
-                    leftcontrol.setAttribute("class", "left carousel-control");
-                    leftcontrol.setAttribute("href", "#myCarousel" + i);
-                    leftcontrol.setAttribute("data-slide", "prev");
-                    carousel2.appendChild(leftcontrol);
-                    var leftarrow = document.createElement("span");
-                    leftarrow.setAttribute("class", "glyphicon glyphicon-chevron-left");
-                    leftcontrol.appendChild(leftarrow);
-                    var leftprevious = document.createElement("span");
-                    leftprevious.setAttribute("class", "sr-only");
-                    leftcontrol.appendChild(leftprevious);
+                    // var leftcontrol = document.createElement("a");
+                    // leftcontrol.setAttribute("class", "left carousel-control");
+                    // leftcontrol.setAttribute("href", "#myCarousel" + i);
+                    // leftcontrol.setAttribute("data-slide", "prev");
+                    // carousel2.appendChild(leftcontrol);
+                    // var leftarrow = document.createElement("span");
+                    // leftarrow.setAttribute("class", "glyphicon glyphicon-chevron-left");
+                    // leftcontrol.appendChild(leftarrow);
+                    // var leftprevious = document.createElement("span");
+                    // leftprevious.setAttribute("class", "sr-only");
+                    // leftcontrol.appendChild(leftprevious);
 
-                    var rightcontrol = document.createElement("a");
-                    rightcontrol.setAttribute("class", "right carousel-control");
-                    rightcontrol.setAttribute("href", "#myCarousel" + i);
-                    rightcontrol.setAttribute("data-slide", "next");
-                    carousel2.appendChild(rightcontrol);
-                    var rightarrow = document.createElement("span");
-                    rightarrow.setAttribute("class", "glyphicon glyphicon-chevron-right");
-                    rightcontrol.appendChild(rightarrow);
-                    var rightnext = document.createElement("span");
-                    rightnext.setAttribute("class", "sr-only");
-                    rightcontrol.appendChild(rightnext);
+                    // var rightcontrol = document.createElement("a");
+                    // rightcontrol.setAttribute("class", "right carousel-control");
+                    // rightcontrol.setAttribute("href", "#myCarousel" + i);
+                    // rightcontrol.setAttribute("data-slide", "next");
+                    // carousel2.appendChild(rightcontrol);
+                    // var rightarrow = document.createElement("span");
+                    // rightarrow.setAttribute("class", "glyphicon glyphicon-chevron-right");
+                    // rightcontrol.appendChild(rightarrow);
+                    // var rightnext = document.createElement("span");
+                    // rightnext.setAttribute("class", "sr-only");
+                    // rightcontrol.appendChild(rightnext);
 
                     //end Carousel
 
@@ -157,7 +157,7 @@ function college() {
                     list.appendChild(websitePath);
                     list.appendChild(items);
                     resultCard.appendChild(mapDiv);
-                    resultCard.appendChild(carousel);
+                    // resultCard.appendChild(carousel);
 
                     initMap();
 
@@ -175,26 +175,49 @@ function college() {
                     var sorted = programsArray.sort(function(prev, curr) {
                         return curr[1] - prev[1];
                     });
-                    var resultsToString = sorted[0].toString();
+                    // for (var i = 0; i < sorted.length; i++) {
+                        var resultsToString = sorted[i].toString();
+                    // }
                     resultsToString = resultsToString.replace("_", " ");
                     resultsToString = capitalize(resultsToString);
                     var strSplit = resultsToString.split(",");
-                    console.log(strSplit)
                     var programList = document.createTextNode(resultsToString);
-
+                    console.log(programList);
+                    // console.log(programList);
                     if (strSplit[1] == 0) {
                         var noResult = document.createTextNode("No Data is Available");
-                        console.log(noResult);
                         items.appendChild(noResult);
-                        // list.innerHTML = programList; -- this appends "Military - 0%"
                     }
                     else if (strSplit[1] == 1) {
+                        if (strSplit[0] == "Parks Recreation_fitness") {
+                            strSplit[0] = "Parks, Recreation, and Fitness";
+                        }
+                        else if (strSplit[0] == "Theology Religious_vocation") {
+                            strSplit[0] = "Theological Studies and Religious Vocations";
+                        }
+                        else if (strSplit[0] == "Public Administration_social_service") {
+                            strSplit[0] = "Public Administration and Social Services";
+                        }
+                        else if (strSplit[0] == "Visual Performing") {
+                            strSplit[0] = "Visual and Performing Arts";
+                        }
                         strSplit[1] = "100%";
-                        var strCool = document.createTextNode(strSplit[0] + " - " + strSplit[1]);
-                        strCool.toString();
-                        items.appendChild(strCool);
+                        var strCool = strSplit[0] + " - " + strSplit[1];
+                        items.innerHTML = strCool;
                     }
                     else if (strSplit[1] < 1){
+                        if (strSplit[0] == "Parks Recreation_fitness") {
+                            strSplit[0] = "Parks, Recreation, and Fitness";
+                        }
+                        else if (strSplit[0] == "Theology Religious_vocation") {
+                            strSplit[0] = "Theological Studies and Religious Vocations";
+                        }
+                        else if (strSplit[0] == "Public Administration_social_service") {
+                            strSplit[0] = "Public Administration and Social Services";
+                        }
+                        else if (strSplit[0] == "Visual Performing") {
+                            strSplit[0] = "Visual and Performing Arts";
+                        }
                         strSplit[1] = strSplit[1] * 100;
                         var firstStrSplit = strSplit[1].toFixed(2);
                         strSplit.toString();
